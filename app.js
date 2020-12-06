881348bd8bfe83fad5ba9ad534bad99466a18406
BANTYC <41577858+BANTYC@users.noreply.github.com>
2020-12-06 18:21:15 +0700

    Added daily heap size dump and mongoose envents

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const zlib = require('zlib');
 const zmq = require('zeromq');
 const sock = zmq.socket('sub');
 const Station = require('./models/station');
+const schedule = require('node-schedule');
+
+schedule.scheduleJob('0 0 * * *', () => {
+  console.log(process.memoryUsage().heapUsed / 1024 / 1024);
+});
 
 const stationNames = new Set();
 Station.getAllStationsNames().then(allStationsDocs => {
@@ -10,6 +15,7 @@ Station.getAllStationsNames().then(allStationsDocs => {
   sock.connect('tcp://eddn.edcd.io:9500');
   console.log('Worker connected to port 9500');
   sock.subscribe('');
+
   sock.on('message', topic => {
     const inflated = JSON.parse(zlib.inflateSync(topic));
     if(inflated.$schemaRef === 'https://eddn.edcd.io/schemas/commodity/3') {
--- modules/mongoose.js
+++ modules/mongoose.js
@@ -3,6 +3,25 @@ const mongoose = require('mongoose');
 const config = require('./../config/index');
 require('dotenv').config();
 
+const connection = mongoose.connection;
+
+connection.on('connecting', () => {
+  console.log('Connecting');
+});
+
+connection.on('connected', () => {
+  console.log('Connected');
+});
+
+connection.on('error', (err) => {
+  console.log('Error: ' + err);
+  mongoose.disconnect();
+});
+
+connection.on('disconnected', () => {
+  console.log('MongoDB disconnected!');
+});
+
 mongoose.connect(process.env.mongoLink, config.get('mongoose').options);
 
 module.exports = mongoose;
