d3c3b32e0c2fd2d9c3ef576243b62a5045b64043
BANTYC <41577858+BANTYC@users.noreply.github.com>
2020-12-20 21:49:11 +0700

    Changed station update and creation: added {upsert: true}

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const zmq = require('zeromq');
 const sock = zmq.socket('sub');
 const schedule = require('node-schedule');
-const Station = require('./models/station');
 const socketEvents = require('./events/socketEvents');
-require('console-stamp')(console, '[HH:MM:ss.l]')
+require('console-stamp')(console, '[HH:MM:ss.l]');
 
 const address = 'tcp://eddn.edcd.io:9500';
 
@@ -11,14 +10,10 @@ schedule.scheduleJob('0 0 * * *', () => {
   console.log(process.memoryUsage().heapUsed / 1024 / 1024);
 });
 
-const stationNames = new Set();
-Station.getAllStationsNames().then(allStationsDocs => {
-  allStationsDocs.map(doc => stationNames.add(doc._doc.stationName));
+sock.connect(address);
+console.log(`Worker connected to ${address}`);
+sock.subscribe('');
 
-  sock.connect(address);
-  console.log(`Worker connected to ${address}`);
-  sock.subscribe('');
+socketEvents.setDisconnectEvent(sock);
+socketEvents.setMessageEvent(sock);
 
-  socketEvents.setDisconnectEvent(sock);
-  socketEvents.setMessageEvent(sock, stationNames);
-});
