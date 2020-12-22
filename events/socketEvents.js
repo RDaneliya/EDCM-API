30060fb08205a6a1385afda82547334033d5be60
BANTYC <41577858+BANTYC@users.noreply.github.com>
2020-12-22 20:32:33 +0700

    Added more events

--- events/socketEvents.js
+++ events/socketEvents.js
@@ -1,19 +1,35 @@
-const zlib = require('zlib')
+const zlib = require('zlib');
 const Station = require('../models/station');
 
+const address = 'tcp://eddn.edcd.io:9500'
+
 module.exports.setMessageEvent = (sock) => {
-    sock.on('message', topic => {
-        const inflated = JSON.parse(zlib.inflateSync(topic));
-        if (inflated.$schemaRef === 'https://eddn.edcd.io/schemas/commodity/3') {
-            const message = inflated.message;
-                Station.update({stationName: message.stationName}, message, {upsert: true});
-        }
-    });
-}
+  sock.on('message', topic => {
+    const inflated = JSON.parse(zlib.inflateSync(topic));
+    if(inflated.$schemaRef === 'https://eddn.edcd.io/schemas/commodity/3') {
+      const message = inflated.message;
+      Station.update({ stationName: message.stationName }, message, { upsert: true });
+    }
+  });
+};
 
 module.exports.setDisconnectEvent = (sock) => {
-    sock.on('disconnect', () => {
-        sock.connect('tcp://eddn.edcd.io:9500');
-        console.log('Disconnected, trying to reconnect');
-    });
+  sock.on('disconnect', () => {
+    console.log('Disconnected, trying to reconnect');
+    sock.connect(address);
+  });
+};
+
+module.exports.setErrorEvent = (sock) => {
+  sock.on('error', (error) => {
+    console.log(error);
+    sock.disconnect();
+  });
+};
+
+module.exports.setExitEvent = (sock) => {
+  sock.on('exit', () =>{
+    console.log('Disconnected, trying to reconnect');
+    sock.connect(address);
+  })
 }
