ba7fa8f21c35068ef6a6eefedf30138abf56a6eb
DaneliyaRI <DaneliyaRI@sibedge.com>
2021-02-16 16:02:10 +0700

    Merged projects to one

--- src/sock/index.js
+++ src/sock/index.js
@@ -1,35 +1,32 @@
 const zlib = require('zlib');
+const zmq = require('zeromq');
+const sock = zmq.socket('sub');
 const Station = require('../models/station');
 
-const address = 'tcp://eddn.edcd.io:9500'
+module.exports = (address, port) => {
+  sock.connect(`${address}:${port}`);
+  sock.subscribe('');
 
-module.exports.setMessageEvent = (sock) => {
   sock.on('message', topic => {
     const inflated = JSON.parse(zlib.inflateSync(topic));
-    if(inflated.$schemaRef === 'https://eddn.edcd.io/schemas/commodity/3') {
+    if (inflated.$schemaRef === 'https://eddn.edcd.io/schemas/commodity/3') {
       const message = inflated.message;
       Station.updateOneUpsert(message);
     }
   });
-};
 
-module.exports.setDisconnectEvent = (sock) => {
   sock.on('disconnect', () => {
     console.log('Disconnected, trying to reconnect');
     sock.connect(address);
   });
-};
 
-module.exports.setErrorEvent = (sock) => {
   sock.on('error', (error) => {
     console.log(error);
     sock.disconnect();
   });
-};
 
-module.exports.setExitEvent = (sock) => {
-  sock.on('exit', () =>{
+  sock.on('exit', () => {
     console.log('Disconnected, trying to reconnect');
     sock.connect(address);
-  })
-}
+  });
+};
