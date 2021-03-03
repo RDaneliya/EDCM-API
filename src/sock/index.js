450b20c942a0068afdffcdb19bf31318e3d2165a
DaneliyaRI <DaneliyaRI@sibedge.com>
2021-03-03 23:04:04 +0700

    Fixed commodities names: agronomictreatment => Agronomic Treatment

--- src/sock/index.js
+++ src/sock/index.js
@@ -2,31 +2,46 @@ const zlib = require('zlib');
 const zmq = require('zeromq');
 const sock = zmq.socket('sub');
 const Station = require('../models/station');
+const delowercase = require('../modules/delowercase');
+
 
 module.exports = (address, port) => {
-  sock.connect(`${address}:${port}`);
-  sock.subscribe('');
+  delowercase.getCommoditiesMap()
+      .then((commoditiesMap) => {
+        sock.connect(`${address}:${port}`);
+        sock.subscribe('');
 
-  sock.on('message', topic => {
-    const inflated = JSON.parse(zlib.inflateSync(topic));
-    if (inflated.$schemaRef === 'https://eddn.edcd.io/schemas/commodity/3') {
-      const message = inflated.message;
-      Station.updateOneUpsert(message);
-    }
-  });
+        sock.on('message', topic => {
+          const inflated = JSON.parse(zlib.inflateSync(topic));
+          if (inflated.$schemaRef === 'https://eddn.edcd.io/schemas/commodity/3') {
+            const message = inflated.message;
 
-  sock.on('disconnect', () => {
-    console.log('Disconnected, trying to reconnect');
-    sock.connect(address);
-  });
+            message.commodities.forEach(commodity => {
+              const commodityEntry = commoditiesMap.get(commodity.name);
+              if (commodityEntry != null) {
+                commodity.name = commodityEntry.name;
+                commodity.category = commodityEntry.category;
+              }
+            });
 
-  sock.on('error', (error) => {
-    console.log(error);
-    sock.disconnect();
-  });
+            Station.updateOneUpsert(message);
+          }
+        });
 
-  sock.on('exit', () => {
-    console.log('Disconnected, trying to reconnect');
-    sock.connect(address);
-  });
+        sock.on('disconnect', () => {
+          console.log('Disconnected, trying to reconnect');
+          sock.connect(address);
+        });
+
+        sock.on('error', (error) => {
+          console.log(error);
+          sock.disconnect();
+        });
+
+        sock.on('exit', () => {
+          console.log('Disconnected, trying to reconnect');
+          sock.connect(address);
+        });
+      });
 };
+
