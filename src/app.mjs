76d98c076ffd7d505ee5d761d27d163be6cfb7c3
BANTYC <BANTYC@DESKTOP>
2021-05-16 18:08:00 +0700

    Fixed problem with path to graphql schema

--- src/app.mjs
+++ src/app.mjs
@@ -1,16 +1,19 @@
 import express from 'express';
-import path from 'path';
-import cookieParser from "cookie-parser";
+import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
-import {graphqlHTTP} from "express-graphql";
-import {loadSchemaSync} from "@graphql-tools/load";
-import {GraphQLFileLoader} from "@graphql-tools/graphql-file-loader";
+import { graphqlHTTP } from 'express-graphql';
+import { loadSchemaSync } from '@graphql-tools/load';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
 import root from './graphql/root.mjs';
-import {Sock} from './sock/index.js';
+import { Sock } from './sock/index.mjs';
+import path from 'path';
 
 
-console.log(path.join(path.resolve(''), '../src/graphql/schemas/schema.graphql'));
-const schema = loadSchemaSync(path.join(path.resolve(''), '../src/graphql/schemas/schema.graphql'), {
+console.log(import.meta.url);
+const currentPath = decodeURI(import.meta.url)
+  .replace(/^file:\/\/\//, '');
+const graphqlSchemaPath = path.join(currentPath, '../graphql/schemas/schema.graphql');
+const schema = loadSchemaSync(graphqlSchemaPath, {
   loaders: [new GraphQLFileLoader()]
 });
 
@@ -18,14 +21,14 @@ const app = express();
 
 app.use(morgan('short'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true
-}))
+}));
 
 Sock('tcp://eddn.edcd.io', 9500);
 
